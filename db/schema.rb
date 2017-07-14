3a605927126f78f8391bc65ac79a8e2ed4032d41
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2017-07-14 13:01:36 +0200

    adding carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714103440) do
+ActiveRecord::Schema.define(version: 20170714104720) do
 
   create_table "pictures", force: :cascade do |t|
     t.string "picture"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20170714103440) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
 end
