159190f87468be88e335b363948fd39c7b74b338
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2017-07-14 14:52:12 +0200

    trying to make cqrrierwave work

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'figaro'
+gem 'cloudinary'
 gem 'carrierwave', '~> 0.11.2'
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ Rails.application.routes.draw do
   get 'home' => 'pages#home'
 
   get 'about' => 'pages#about'
+
+  get 'show' => 'products#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
