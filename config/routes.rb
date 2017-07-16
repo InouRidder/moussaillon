8ace5f3965664752625a6632939dfc2104d83cb7
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2017-07-16 17:50:29 +0200

    inou

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  get 'contact' => 'pages#contact'
+  get '/contact' => 'pages#contact'
 
-  get 'home' => 'pages#home'
+  get '/home' => 'pages#home'
 
-  get 'about' => 'pages#about'
+  get '/about' => 'pages#about'
 
-  get 'show' => 'products#show'
+  get '/show' => 'products#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
