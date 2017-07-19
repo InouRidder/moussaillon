79f4038cb58ee16c215ebff303afb09e088bc4fd
Inou Ridder <inouridder@gmail.com>
2017-07-19 17:36:25 +0200

    working on results page, wrong brach sorry

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   get '/show' => 'products#show'
 
-  get '/results' => 'products#results'
+  get '/results' => 'pages#results'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
