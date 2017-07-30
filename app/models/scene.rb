9b572a78ea12ce16aa07bf2159b925d7b57e0985
Inou Ridder <inouridder@gmail.com>
2017-07-30 22:39:44 +0100

    add banner uploader for scene

--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,5 +1,10 @@
 class Scene < ApplicationRecord
   has_many :products
 
+  mount_uploader :banner, BannerUploader
+
   validates :banner, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+
 end
