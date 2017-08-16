9afd3f9f179633400d81ff28c96f4b16d5717f93
Inou Ridder <inouridder@gmail.com>
2017-08-16 15:10:16 +0200

    add link index to show client, fix routing for button on admin product index

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,53 +1,55 @@
-class AdminProductsController < ApplicationController
-  layout 'admin'
-  before_action :authenticate_user!
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
+module Admin
+  class ProductsController < ApplicationController
+    layout 'admin'
+    before_action :authenticate_user!
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
 
-  def new
-    @product = Product.new
-  end
-
-  def create
-   @product = Product.new(product_params)
-    if @product.save
-      redirect_to admin_product_path(@product)
-    else
-      render :new
+    def new
+      @product = Product.new
     end
+
+    def create
+     @product = Product.new(product_params)
+      if @product.save
+        redirect_to admin_product_path(@product)
+      else
+        render :new
+      end
+    end
+
+    def index
+      @products = Product.all
+    end
+
+    def show
+      # @product = Product.find(params[:id])
+    end
+
+    def edit
+      # @product = Product.find(params[:id])
+    end
+
+    def update
+      # @product = product.find(params[:id])
+      @product.update(product_params)
+      redirect_to admin_product_path(@product)
+    end
+
+    def destroy
+      # @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to admin_products_path
+    end
+
+    private
+
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:name, :description, :price, :photo, :photo_cache)
+    end
+
   end
-
-  def index
-    @products = Product.all
-  end
-
-  def show
-    # @product = Product.find(params[:id])
-  end
-
-  def edit
-    # @product = Product.find(params[:id])
-  end
-
-  def update
-    # @product = product.find(params[:id])
-    @product.update(product_params)
-    redirect_to admin_product_path(@product)
-  end
-
-  def destroy
-    # @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to admin_products_path
-  end
-
-private
-
-  def set_product
-    @product = Product.find(params[:id])
-  end
-
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :photo, :photo_cache)
-  end
-
 end
