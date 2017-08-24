b157f31d2937949c4814e3ceb3ffa3f368c97556
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2017-08-24 12:28:53 +0200

    working on the scenes

--- app/controllers/admin/scenes_controller.rb
+++ app/controllers/admin/scenes_controller.rb
@@ -1,6 +1,8 @@
 module Admin
   class ScenesController < BaseController
-
+  layout 'admin'
+  before_action :authenticate_user!
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
     def new
       @scene = Scene.new
     end
@@ -19,19 +21,36 @@ module Admin
     end
 
     def show
-      @scene = Scene.find(params[:id])
+      @products = Product.all - @scene.products
+      # @scene = Scene.find(params[:id])
     end
 
     def edit
+      # @scene = Scene.find(params[:id])
     end
 
     def update
+      # @scene = Scene.find(params[:id])
+      if product_id = params["scene"]["products"]
+        product = Product.find(product_id)
+        product.scene = @scene
+        product.save
+      end
+      @scene.update(scene_params)
+      redirect_to admin_scene_path(@scene)
     end
 
     def destroy
+       # @scene = Scene.find(params[:id])
+      @scene.destroy
+      redirect_to admin_scenes_path
     end
 
   private
+
+     def set_product
+      @scene = Scene.find(params[:id])
+    end
 
     def scene_params
       params.require(:scene).permit(:title, :description, :banner, :banner_cache)
