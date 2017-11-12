37245b7f2a698e04a05cb6a191b7439cf63f4556
Inou Ridder <inouridder@gmail.com>
2017-11-12 21:36:56 +0100

    changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @latest_products = Product.last(2)
-    # @products_banner = @products.banner
+    @words = ["Tailor made", "Leather", "Useful", "Timeless","Top","of","the","range","Robust","Local","Taste","Shape","Carved","Beautiful","Hardwood","Noble","Stones","Perfect","Unique"]
   end
 
   def about
