96eac2a124dc909a4eaf1529eaf11e0b5b3b5d26
Inou Ridder <inouridder@gmail.com>
2017-11-16 21:37:32 +0100

    back to words

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 
   def home
     @latest_products = Product.last(2)
+    @recent_scenes = Scene.last_2
     @words = ["Tailor made", "Leather", "Useful", "Timeless","Top","of","the","range","Robust","Local","Taste","Shape","Carved","Beautiful","Hardwood","Noble","Stones","Perfect","Unique"]
   end
 
--- app/models/scene.rb
+++ app/models/scene.rb
@@ -1,7 +1,8 @@
 class Scene < ApplicationRecord
   has_many :products, dependent: :nullify
   has_attachment :banner
-  # validates :banner, presence: true
+
+  scope :last_2, -> { order(created_at: :desc).first(2) }
 
   validates :title, presence: true
   validates :description, presence: true
