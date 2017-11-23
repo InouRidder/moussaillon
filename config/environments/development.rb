9553f5f815e7a6e72f6ec8635cfc907a7b5d587b
Inou Ridder <inouridder@gmail.com>
2017-11-23 20:23:28 +0100

    changed video to local

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = false
+  config.assets.paths << "#{Rails.root}/app/assets/videos"
 
   # Suppress logger output for asset requests.
   config.assets.quiet = false
