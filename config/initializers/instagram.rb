ec7ca840f5940b634fa5e07a5ef9f3d4a4f19c18
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2019-10-15 17:56:22 +0200

    instagram api connected and configurated

---
+++ config/initializers/instagram.rb
@@ -0,0 +1,6 @@
+
+require "instagram"
+Instagram.configure do |config|
+config.client_id =  "4c3b64b46b5f4555875bca1b0677da10"
+config.access_token = "3317896663.4c3b64b.b4e278d37aef4969b0d2a084380cd7d3"
+end
