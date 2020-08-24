b3ef6ba8c0faa1d642a041dc9281556aad74e170
Inou <Ridder>
2020-08-24 13:52:41 +0200

    quickfix remove api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,10 +6,10 @@ class PagesController < ApplicationController
   # end
 
   def home
-    url = open("https://api.instagram.com/v1/users/self/media/recent/?access_token=#{ENV['INSTAGRAM_TOKEN']}").read
-    @object =  JSON.parse(url)
-    @latest_products = Product.last(2)
-    @recent_scenes = Scene.last_2
+    # url = open("https://api.instagram.com/v1/users/self/media/recent/?access_token=#{ENV['INSTAGRAM_TOKEN']}").read
+    # @object =  JSON.parse(url)
+    # @latest_products = Product.last(2)
+    # @recent_scenes = Scene.last_2
     @words = ["Tailor made", "Leather", "Useful", "Timeless","Top","of","the","range","Robust","Local","Taste","Shape","Carved","Beautiful","Hardwood","Noble","Stones","Perfect","Unique"]
   end
 
